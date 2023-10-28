e6d10a0f11d51465f34dafe094252b360bb3b9cd
rumians <ruanfort12@gmail.com>
2023-10-28 20:58:37 -0300

    responsividade

--- menu.js
+++ menu.js
@@ -1,5 +1,17 @@
 window.addEventListener("scroll", function() {
-    let menu = document.querySelector('#menuzin');
+    let menu = document.querySelector('#nav-bar');
     menu.classList.toggle('rolagem', window.scrollY > 0);
-  });
+});
+
+
+function  menulateral() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "midia/menu-icon-removebg-preview.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "midia/x.png";
+    }
+}
   
